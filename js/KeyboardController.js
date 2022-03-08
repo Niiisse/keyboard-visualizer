1e634ecc8afb062d0f32c9e966467172464e4765
Nisse Lommerde <nisselommerde@gmail.com>
2022-03-08 15:24:55 +0100

    Documented functions

--- js/KeyboardController.js
+++ js/KeyboardController.js
@@ -6,15 +6,28 @@ import KeyboardShortcut from "./KeyboardShortcut.js";
   * Keyboard Controller
   * Handles keyboard events
   * TODO: re-scan pressed mofifiers when a key is let go
+  * TODO: methods for adding (or potentially removing) KeyboardShortcuts
   */
 export default class KeyboardController {
+   /**
+     * @constructor
+     * Sets up necessary classes
+     */
     constructor() {
         this.modifierKeys = new ModifierKeys();
         this.oskInterface = new OskInterface();
         this.keyboardShortcuts = new Array();
         this.generateTestArray();
     }
-
+    /**
+      * Handles key down logic
+      * Set active class for pressed button
+      * For each keyboardShortcut, see if the associated keys
+      * match the currently pressed modifier keys
+      * If all match, activate up key on OskInterface
+      *
+      * @param {string} key the currently pressed key
+      */
     keyDown(key) {
         this.allKeysUp();
 
@@ -27,46 +40,58 @@ export default class KeyboardController {
         this.keyboardShortcuts.forEach(shortcut => {
             // Compare shortcut modifierkeys amoutn to currently pressed modifierkeys
             let breakMatch = false;
-            if (shortcut.modifierKeys.length == this.modifierKeys.getModifiersAmount()) {
-                // Amounts match -- compare keys to currently pressed keys
-                shortcut.modifierKeys.forEach(modifierKey => {
-                    if (!this.modifierKeys.isModifierSet(modifierKey)) {
-                        breakMatch = true;
-                    }
-                });
-
-                if (!breakMatch) {
-                    // Match! Show shortcut
-                    this.oskInterface.activateButton(shortcut.getKey(), shortcut.getColorData());
-                    console.info(`Matched, showing key ${shortcut.modifierKeys} + ${shortcut.getKey()}, ${shortcut.getColorData()}`);
+            // Amounts match -- compare keys to currently pressed keys
+            shortcut.modifierKeys.forEach(modifierKey => {
+                if (!this.modifierKeys.isModifierSet(modifierKey)) {
+                    breakMatch = true;
                 }
+            });
+
+            if (!breakMatch) {
+                // Match! Show shortcut
+                this.oskInterface.activateButton(shortcut.getKey(), shortcut.getColorData());
+                console.info(`Matched, showing key ${shortcut.modifierKeys} + ${shortcut.getKey()}, ${shortcut.getColorData()}`);
             }
         });
     }
-
+    /**
+      * Handles key up event
+      * Keeps track of modifier state, if relevant
+      * Removes active class from key, scans all keyboardShortcuts and removes
+      * active class from relevant keys as well
+      * Removes pressed key's active state, just in case
+      *
+      * @param {string} key the currently pressed key
+      */
     keyUp(key) {
-        // Keep track of modifier states
         if (this.isKeyModifier(key)) {
             this.modifierKeys.setModifier(key, false);
+            this.keyboardShortcuts.forEach(shortcut => {
+                if (shortcut.getModifiers().includes(key)) {
+                    this.oskInterface.deactivateButton(shortcut.getKey());
+                }
+            });
         }
 
-        // Disable key
         this.oskInterface.deactivateButton(key, "active");
-
-        // Disable any shortcuts associated with the keyUp
-        this.keyboardShortcuts.forEach(shortcut => {
-            if (shortcut.getModifiers().includes(key)) {
-                this.oskInterface.deactivateButton(shortcut.getKey());
-            }
-        });
     }
 
+    /**
+      * Disables each key. Used to make sure there's no ghost keys remaning when
+      * pressing additional modifiers
+      */
     allKeysUp() {
         this.keyboardShortcuts.forEach(shortcut => {
             this.keyUp(shortcut.getKey());
         });
     }
 
+    /**
+      * Checks whether key is a modifier
+      *
+      * @param {string} key Pressed key
+      * @returns {bool} true or false
+      */
     isKeyModifier(key) {
         if (key == 'Control' || key == 'Super' || key == 'Alt' || key == 'Shift') {
             return true;
